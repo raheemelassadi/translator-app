2734423b8d5db8db93bc1fa4971015bb8562b29d
raheemelassadi1@gmail.com <raheemelassadi1@gmail.com>
2024-03-02 16:06:23 -0800

    formatted body for input text container

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,45 @@ import viteLogo from "/vite.svg";
 function App() {
   return (
     <>
-      <body className="flex justify-center flex-col items-center h-screen">
-        <div id="input-box" className="w-2/6 bg-lightBlue rounded-xl">
-          <ul className="flex justify-between w-96 gap-11">
-            <li>Detect Language</li>
-            <li>English</li>
-            <li>French</li>
+      <body
+        id="body"
+        className="flex justify-center flex-col items-center h-screen"
+      >
+        <div id="input-box" className=" bg-lightBlue rounded-3xl">
+          <ul className="flex gap-11 px-14 py-7 text-lightGray text-lg font-medium">
+            <li className="cursor-pointer">Detect Language</li>
+            <li className="cursor-pointer">English</li>
+            <li className="cursor-pointer">French</li>
             <li>
-              <select name="" id="">
+              <select name="" id="" className="mr-60">
                 <option value="spanish">Spanish</option>
+                <option value="german">German</option>
               </select>
             </li>
           </ul>
+          <hr className="m-auto text-lightgray" />
+          <div id="text-input">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="w-full resize-none p-7"
+            ></textarea>
+            <p className="text-right px-7">19/500</p>
+          </div>
+          <footer className="flex justify-between p-7">
+            <div>
+              <button>Play Sound</button>
+              <button>Copy</button>
+            </div>
+            <div>
+              <button>Translate</button>
+            </div>
+          </footer>
           <hr />
         </div>
+
         <div id="translate-box"></div>
       </body>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     colors: {
-      lightBlue: "#121826cc",
+      lightBlue: "#121826B3",
+      lightGray: "#394150",
     },
     extend: {},
   },
